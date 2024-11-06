ef09c5c218f8c7fc608422a4844d22fc0fef2fc4
SigmaZardan <bibekbhujel077@gmail.com>
2024-11-06 12:25:02 +0545

    Initial Commit

---
+++ Amigos/AmigosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmigosApp.swift
+//  Amigos
+//
+//  Created by Bibek Bhujel on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmigosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
